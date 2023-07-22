64687cab120c5be8bee5b97e415352730ae75b2d
Harishjallu <itsharish369@gmail.com>
2023-07-22 21:19:17 +0530

    local storage API was added

---
+++ localStorage API/main.js
@@ -0,0 +1,35 @@
+const toggle = document.getElementById('toggle-mode');
+
+
+
+const mode = localStorage.getItem('theme');
+console.log(mode);
+
+if(mode){
+    applyTheme(mode)
+}else{
+    applyTheme('light-mode');
+}
+
+function applyTheme(theme) {
+    if( theme === 'dark-mode'){
+        document.body.classList.remove('light-mode');
+        document.body.classList.add('dark-mode');
+
+    }else{
+        document.body.classList.remove('dark-mode');
+        document.body.classList.add('light-mode');
+    }
+    localStorage.setItem('theme', theme);
+}
+
+function toggleTheme(){
+    const currentTheme = document.body.classList.contains('dark-mode') ? 'dark-mode' : 'light-mode';
+    const newTheme = currentTheme === 'dark-mode' ? 'light-mode' : 'dark-mode';
+    applyTheme(newTheme);
+}
+
+
+toggle.addEventListener('click', toggleTheme);
+
+
