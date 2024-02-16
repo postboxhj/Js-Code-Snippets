1da6fe8d395ca3e1683fdd664254cbe9d4e79f9c
Harish <116578655+postboxhj@users.noreply.github.com>
2024-02-16 16:50:01 +0530

    Fetch API

--- Fetch API/main.js
+++ Fetch API/main.js
@@ -1,84 +1,16 @@
+//FETCH API
+const FETCH_API_URL = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.9890648&lng=82.2474648&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
 
-
-
-// const url = 'https://jsonplaceholder.typicode.com/users';
-
-// const message = document.getElementById('message');
-// const userList = document.getElementById('user-list');
-
-// async function fetchData() {
-//   try {
-//     const response = await fetch(url);
-    
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
-
-//     const data = await response.json();
-
-//     userList.innerHTML = '';
-
-//     data.forEach(userData => {
-//       const listItem = document.createElement('li');
-//       listItem.innerHTML = `
-//         <p><strong>ID:</strong> ${userData.id}</p>
-//         <p><strong>Name:</strong> ${userData.name}</p>
-//         <p><strong>Username:</strong> ${userData.username}</p>
-//         <p><strong>Email:</strong> ${userData.email}</p>
-//         <p><strong>Address:</strong> ${userData.address.street}, ${userData.address.suite}, ${userData.address.city}, ${userData.address.zipcode}</p>
-//         <p><strong>Geo Location:</strong> Latitude: ${userData.address.geo.lat}, Longitude: ${userData.address.geo.lng}</p>
-//         <p><strong>Phone:</strong> ${userData.phone}</p>
-//         <p><strong>Website:</strong> ${userData.website} </p>
-//         <p><strong>Company Name:</strong> ${userData.company.name}</p>
-//         <p><strong>Catchphrase:</strong> ${userData.company.catchPhrase}</p>
-//         <p><strong>Company BS:</strong> ${userData.company.bs}</p>`;
-
-//       userList.appendChild(listItem);
-//     });
-//   } catch (error) {
-//     console.error('Error fetching data:', error);
-//   }
-// }
-
-// fetchData();
-
-
-
-const url = 'https://jsonplaceholder.typicode.com/photos';
-
-const message = document.querySelector('#message');
-const userList = document.querySelector('#user-list');
-
-
-async function fetchData(){
-
-    try{
-
-        const response = await fetch(url);
-    if(!response.ok){
-        throw new Error('Fetching data failed');
+const fetchAPI = async () => {
+  try {
+    const data = await fetch(FETCH_API_URL);
+    if (!data.ok) {
+      throw new Error(`Fetching ${FETCH_API_URL} failed`);
     }
-
-    const data = await response.json();
-
-    userList.innerHTML = '';
-
-    data.forEach(user => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = 
-        `<p><strong>User Id :</strong> ${user.albumId} </p>
-        <p><strong>Id :</strong> ${user.id} </p>
-        <p><strong>Title :</strong></strong> ${user.title} </p>
-        <p><strong>Url :</strong> ${user.url} </p>
-        <img src="${user.thumbnailUrl}" alt="Thumbnail">`;
-        userList.appendChild(listItem);
-    });
-
-    }
-    catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    
-}
-
-fetchData();
+    const json = await data.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error);
+  }
+};
+fetchAPI();
